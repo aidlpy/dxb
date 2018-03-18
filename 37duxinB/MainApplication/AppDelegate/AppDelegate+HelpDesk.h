0de5b8addfc0cc6a438688d2503990537ceb90b9
Zhang Xinrong <zhang651651@hotmail.com>
2018-03-18 20:14:34 +0800

    B_part5

---
+++ 37duxinB/MainApplication/AppDelegate/AppDelegate+HelpDesk.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate+EaseMob.h
+//  EasMobSample
+//
+//  Created by dujiepeng on 12/5/14.
+//  Copyright (c) 2014 dujiepeng. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "Header.h"
+#import "MainTabbarVC.h"
+
+@interface AppDelegate (HelpDesk)<UIAlertViewDelegate>
+@property(nonatomic,retain)MainTabbarVC *tabbar;
+- (void)easemobApplication:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
+-(void)userAccountDidRemoveFromServer ;
+-(void)userAccountDidLoginFromOtherDevice;
+
+@end
---
+++ 37duxinB/MainApplication/AppDelegate/AppDelegate+HelpDesk.m
@@ -0,0 +1,259 @@
+//
+//  AppDelegate+EaseMob.m
+//  EasMobSample
+//
+//  Created by dujiepeng on 12/5/14.
+//  Copyright (c) 2014 dujiepeng. All rights reserved.
+//
+
+#import "AppDelegate+HelpDesk.h"
+#import "AppDelegate+GETUI.h"
+#import "LocalDefine.h"
+#import <Bugly/Bugly.h>
+
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+#import <UserNotifications/UserNotifications.h>
+#endif
+
+/**
+ *  本类中做了EaseMob初始化和推送等操作
+ */
+
+@implementation AppDelegate (HelpDesk)
+- (void)easemobApplication:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+        [self initIMchat];
+        AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+        [audioSession overrideOutputAudioPort:AVAudioSessionPortOverrideSpeaker error:nil];
+        [audioSession setActive:YES error:nil];
+    
+        /*
+         环信的注册和登录
+         */
+        // 环信注册和登录
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(registerIMChat) name:LOGINEMUSER object:nil];
+    
+        /*
+         注册IM用户【注意:注册建议在服务端创建，而不要放到APP中，可以在登录自己APP时从返回的结果中获取环信账号再登录环信服务器。】
+         */
+        //注册环信监听
+        [self setupNotifiers];
+    
+        [self getuiApplication:application didFinishLaunchingWithOptions:launchOptions];
+    
+
+}
+
+-(void)initIMchat{
+    
+    EMOptions *options = [EMOptions optionsWithAppkey:@"1110180119178117#37duxin"];
+    options.enableConsoleLog = YES;
+    options.apnsCertName = @"37duxincdevelopment";
+    [[EMClient sharedClient] initializeSDKWithOptions:options];
+    
+    //ios8注册apns
+    [self registerRemoteNotification];
+    
+    
+    [self registerIMChat];
+
+}
+
+-(void)registerIMChat{
+    
+    [[EMClient sharedClient] logout:YES];
+    NSString *username = FetchEMUsername;
+    NSString *emps = FetchEMPassword;
+    
+   // [[EMClient sharedClient] registerWithUsername:FetchEMUsername password:FetchEMPassword];
+    //登录使用user123 和 iostest 进行测试 密码均为123456 进去serviceModule 找到 ServiceViewController并找到 chatwithPaymentVC控制器。
+    [[EMClient sharedClient] loginWithUsername:username password:emps completion:^(NSString *aUsername, EMError *aError) {
+        if (!aError) {
+            [[EMClient sharedClient].options setIsAutoLogin:YES];
+            NSLog(@"登录成功-----");
+        } else {
+            NSLog(@"aError.code==>%ld",(long)aError.code);
+            NSLog(@"登录失败----%@", aError.errorDescription);
+        }
+    }];
+
+}
+
+
+// 监听系统生命周期回调，以便将需要的事件传给SDK
+- (void)setupNotifiers{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appDidEnterBackgroundNotif:)
+                                                 name:UIApplicationDidEnterBackgroundNotification
+                                               object:nil];
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appWillEnterForeground:)
+                                                 name:UIApplicationWillEnterForegroundNotification
+                                               object:nil];
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appDidFinishLaunching:)
+                                                 name:UIApplicationDidFinishLaunchingNotification
+                                               object:nil];
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appDidBecomeActiveNotif:)
+                                                 name:UIApplicationDidBecomeActiveNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appWillResignActiveNotif:)
+                                                 name:UIApplicationWillResignActiveNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appDidReceiveMemoryWarning:)
+                                                 name:UIApplicationDidReceiveMemoryWarningNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appWillTerminateNotif:)
+                                                 name:UIApplicationWillTerminateNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appProtectedDataWillBecomeUnavailableNotif:)
+                                                 name:UIApplicationProtectedDataWillBecomeUnavailable
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appProtectedDataDidBecomeAvailableNotif:)
+                                                 name:UIApplicationProtectedDataDidBecomeAvailable
+                                               object:nil];
+}
+
+
+-(void)appDidEnterBackgroundNotif:(NSNotification*)notif{
+    
+    
+}
+
+-(void)appWillEnterForeground:(NSNotification *)notif{
+    
+    
+}
+
+
+- (void)appDidFinishLaunching:(NSNotification*)notif
+{
+//    [[HChatClient sharedClient] applicationdidfinishLounching];
+ //   [[EaseMob sharedInstance] applicationDidFinishLaunching:notif.object];
+}
+
+- (void)appDidBecomeActiveNotif:(NSNotification*)notif
+{
+  //  [[EaseMob sharedInstance] applicationDidBecomeActive:notif.object];
+}
+
+- (void)appWillResignActiveNotif:(NSNotification*)notif
+{
+ //   [[EaseMob sharedInstance] applicationWillResignActive:notif.object];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"closeRecording" object:nil];
+}
+
+- (void)appDidReceiveMemoryWarning:(NSNotification*)notif
+{
+ //   [[EaseMob sharedInstance] applicationDidReceiveMemoryWarning:notif.object];
+}
+
+- (void)appWillTerminateNotif:(NSNotification*)notif
+{
+//    [[EaseMob sharedInstance] applicationWillTerminate:notif.object];
+}
+
+- (void)appProtectedDataWillBecomeUnavailableNotif:(NSNotification*)notif
+{
+}
+
+- (void)appProtectedDataDidBecomeAvailableNotif:(NSNotification*)notif
+{
+}
+
+
+/** 注册远程通知 */
+- (void)registerRemoteNotification {
+    /*
+     警告：Xcode8的需要手动开启“TARGETS -> Capabilities -> Push Notifications”
+     */
+    
+    /*
+     警告：该方法需要开发者自定义，以下代码根据APP支持的iOS系统不同，代码可以对应修改。
+     以下为演示代码，注意根据实际需要修改，注意测试支持的iOS系统都能获取到DeviceToken
+     */
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 10.0) {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0 // Xcode 8编译会调用
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        center.delegate = self;
+        [center requestAuthorizationWithOptions:(UNAuthorizationOptionBadge | UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionCarPlay) completionHandler:^(BOOL granted, NSError *_Nullable error) {
+            if (!error) {
+                NSLog(@"request authorization succeeded!");
+            }
+            else
+            {
+                NSLog(@"request authorization error!");
+            }
+            
+            
+            
+        }];
+        
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+#else // Xcode 7编译会调用
+        UIUserNotificationType types = (UIUserNotificationTypeAlert | UIUserNotificationTypeSound | UIUserNotificationTypeBadge);
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+#endif
+    } else if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0) {
+        UIUserNotificationType types = (UIUserNotificationTypeAlert | UIUserNotificationTypeSound | UIUserNotificationTypeBadge);
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+    } else {
+        UIRemoteNotificationType apn_type = (UIRemoteNotificationType)(UIRemoteNotificationTypeAlert |
+                                                                       UIRemoteNotificationTypeSound |
+                                                                       UIRemoteNotificationTypeBadge);
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:apn_type];
+    }
+}
+
+#pragma mark - App Delegate
+
+// 将得到的deviceToken传给SDK
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        if(deviceToken){
+            CacheDeviceToken(deviceToken);
+        }
+        [[EMClient sharedClient] bindDeviceToken:deviceToken];
+        [self getTuiApplication:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+    });
+}
+
+// 注册deviceToken失败，此处失败，与环信SDK无关，一般是您的环境配置或者证书配置有误
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"apns.failToRegisterApns", Fail to register apns)
+                                                    message:error.description
+                                                   delegate:nil
+                                          cancelButtonTitle:NSLocalizedString(@"ok", @"OK")
+                                          otherButtonTitles:nil];
+    [alert show];
+    [self getTuiApplication:application didFailToRegisterForRemoteNotificationsWithError:error];
+    
+}
+
+
+
+@end
