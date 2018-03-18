9003ca10fb0ede55fcbdcd58ac02608bb47e9d3f
Zhang Xinrong <zhang651651@hotmail.com>
2018-03-18 17:39:31 +0800

    B_part5

---
+++ 37duxinB/MainApplication/GeneralModule/BaseViewController/BaseViewController.h
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.h
+//  mtm
+//
+//  Created by Albert on 18/01/2017.
+//  Copyright © 2017 guest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Header.h"
+#import "CustomNavView.h"
+
+@interface BaseViewController : UIViewController
+@property(nonatomic,retain)CustomNavView *navView;
+-(void)setStatusBarBackgroundColor:(UIColor *)color;
+
+@end
---
+++ 37duxinB/MainApplication/GeneralModule/BaseViewController/BaseViewController.m
@@ -0,0 +1,74 @@
+//
+//  BaseViewController.m
+//  mtm
+//
+//  Created by Albert on 18/01/2017.
+//  Copyright © 2017 guest. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+
+@interface BaseViewController ()
+{
+   
+}
+@end
+
+@implementation BaseViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    if ([self respondsToSelector:@selector(setEdgesForExtendedLayout:)])
+    {
+        [self setEdgesForExtendedLayout:UIRectEdgeNone];
+    }
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self.navigationController.navigationBar setHidden:YES];
+    _navView = [[CustomNavView alloc] init];
+    _navView.backgroundColor = Color_5DCBF5;
+    __block typeof(self) weakSelf = self;
+    _navView.backBlock =^(){[weakSelf backTo];};
+    _navView.pushBlock = ^(){[weakSelf nextTo];};
+    [self.view addSubview:_navView];
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBar.hidden = YES;
+    
+}
+
+//设置状态栏颜色
+-(void)setStatusBarBackgroundColor:(UIColor *)color {
+    
+    UIView *statusBar = [[[UIApplication sharedApplication] valueForKey:@"statusBarWindow"] valueForKey:@"statusBar"];
+    if ([statusBar respondsToSelector:@selector(setBackgroundColor:)]) {
+        statusBar.backgroundColor = color;
+    }
+}
+
+
+-(void)nextTo{};
+-(void)backTo{};
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
